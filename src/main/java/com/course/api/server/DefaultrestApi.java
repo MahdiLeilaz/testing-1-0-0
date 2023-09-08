b171f20a114d29d1978bd536197cd67270f8d3ba
Mahdi_Leilaz <mahdi_leilaz@yahoo.com>
2023-09-08 18:22:17 +0330

    Add log to DefaultrestApi
    Log Body .....
    ..............

--- src/main/java/com/course/api/server/DefaultrestApi.java
+++ src/main/java/com/course/api/server/DefaultrestApi.java
@@ -2,6 +2,8 @@ package com.course.api.server;
 
 import java.time.LocalTime;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/api")
 public class DefaultrestApi {
+	
+	private Logger LOG = LoggerFactory.getLogger(DefaultrestApi.class);
 
 	@GetMapping(value = "/welcome")
 	public String welcome() {
+		LOG.info("someone requested welcome api !");
 		return "Welcome to Spring Rest API !";
 	}
 	
